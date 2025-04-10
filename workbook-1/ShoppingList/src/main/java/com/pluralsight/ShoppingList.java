706d45b7c330955300067a22fc152fa70604745a
KayahMcVay <kayahjosephine@gmail.com>
2025-04-10 17:19:47 -0400

    Initial Commit

---
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Pretzels (1-Snack Pack)");
+        System.out.println("Burgers (6-Pack)");
+        System.out.println("Clementines (1Ib-Bag)");
+        System.out.println("Cherry Soda (4-Pack)");
+        System.out.println("Shea Moisture Hair Products (3-Pack)");
+        System.out.println("Iphone USB-C Charger (1-Pack) ");
+        System.out.println("Lasagna (Family-Size)");
+        System.out.println("Blueberry Muffins (4-Pack) ");
+
+    }
+}
---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello I am K'ayah and I love pizza!");
+    }
+}
